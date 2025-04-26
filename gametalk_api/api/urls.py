15924b721b007545169d01d99463526c2fb7ec76
zhaqsylyqq <neennn36@gmail.com>
2025-04-26 06:14:16 +0600

    Сохранил текущую версию в temp-backup

--- gametalk_api/api/urls.py
+++ gametalk_api/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import GameViewSet, ReviewViewSet, DiscussionViewSet, MessageViewSet, ProfileViewSet
-
+from . import views
 router = DefaultRouter()
 router.register(r'games', GameViewSet)
 router.register(r'reviews', ReviewViewSet)
@@ -14,4 +14,17 @@ urlpatterns = [
     path('', include(router.urls)),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('games/', views.GameList.as_view(), name='game-list'),
+    path('games/<int:pk>/', views.GameDetail.as_view(), name='game-detail'),
+
+    # Reviews
+    path('games/<int:game_id>/reviews/', views.ReviewList.as_view(), name='review-list'),
+    path('reviews/', views.ReviewCreate.as_view(), name='review-create'),
+
+    # Discussions
+    path('discussions/', views.DiscussionList.as_view(), name='discussion-list'),
+    path('games/<int:game_id>/discussions/', views.GameDiscussionList.as_view(), name='game-discussion-list'),
+
+    # Messages
+    path('messages/', views.MessageList.as_view(), name='message-list'),
 ]
--- gametalk_api/gametalk/settings.py
+++ gametalk_api/gametalk/settings.py
@@ -57,10 +57,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',  # Временно разрешим доступ всем
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ]
 }
-
 # Настройки JWT
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
